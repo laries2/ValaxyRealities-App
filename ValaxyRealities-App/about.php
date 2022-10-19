159e7a18e1ab972b6f38b1bdb5b432a56ab4d342
laries2 <lariesbett8@gmail.com>
2022-10-19 17:27:43 +0300

    ValaxyRealities-App

---
+++ ValaxyRealities-App/about.php
@@ -0,0 +1,64 @@
+<?php include 'includes/header.php';?>
+        <!-- Navigation Bar -->
+   <?php include 'includes/navbar.php';?>
+        <!-- Navigation Bar -->
+
+    <div class="container">
+        <div class="row">
+	        <!-- Page Content -->
+	        <div class="col-md-8">
+            <h1 class="page-header">About | Us </h1>
+            Hi there!
+            <br>
+            This application is a Video and image content publishing system which allows users to post and manage various kinds of news content.<br
+            <br><br><br>
+
+
+
+
+           
+           Main Features:
+           <br><br>
+           # Multiple user access: allows multiple type of users to login <br>
+           # Functional Admin panel:  allows all admins to manage their content properly with admin panel  <br>
+           # CRUD functionalities:  allows all users to create,read,update and delete their content in a managed format <br>
+           # Profile update option: allows users to update their profile/account details <br>
+           # Secure registration and login option for users <br>
+           # Trending news updates:  allows user to view all trending news around the world (using google news api) <br>
+           # Search news:  option for search all content 
+           <br><br><br>
+
+           Tools used: 
+           <br><br>
+           #Front-End:  HTML, CSS <br>
+           #Back-End:   PHP       <br>
+           #DataBase:   MySQL     <br>
+           <br><br><br>
+
+           Developed By: <br><br>
+           Name: Hillary Bett <br>
+           Email: lariesbett8@gmail.com <br>
+           Website: https://laries.netlify.app/ <br>
+           <br>
+            In association with:  <br><br>
+            Name: Valentine Kagweria<br>
+            Email: valentinekagweria@gmail.com    <br>
+           <br>
+
+
+
+        </div>
+
+             <div class="col-md-4">
+
+               <?php include 'includes/sidebar.php';
+?><!-- Footer -->
+</div>
+</div>
+</div>
+<?php include 'includes/footer.php';?>
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ ValaxyRealities-App/includes/footer.php
@@ -0,0 +1,9 @@
+ <footer class="foo">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div id="footer" style="margin-top:20px; width:100%; background: purple; color: white; height: 40px;">
+                        <p class="text-center" style="padding-top: 10px;">Copyright &copy; 2022 | Valaxy Realities | All rights reserved</p>
+                    </div>
+                     </div>
+            </div>
+   </footer>
